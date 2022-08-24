59956da15fc7a6305d0ccbea73f452a4281b1c8a
abhi2229 <53570437+abhi2229@users.noreply.github.com>
2022-08-24 21:56:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ function compute()
     var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
     
     var resultDisplay = document.getElementById("result");
-    resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
+    var total = principal + interest;
+    resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + total + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
 }
 
 function SliderValue()
